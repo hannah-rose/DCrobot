0aed3c5d48a322dd780c9b2a42ae78c927a45079
Hannah Arntson <hannaharntson@gmail.com>
2015-05-07 12:27:14 -0500

    Working on user button

--- detection.X/detect_cube.c
+++ detection.X/detect_cube.c
@@ -3,11 +3,12 @@
 
 void setup() {
     TRISBbits.TRISB3=0;  //configure LD1 as output
+    TRISBbits.TRISB4=1;  //BT1 is an input
     LD1=1; //turn LD1 off
 }
 
 void loop() {
-  if (USER) {
-    LD1=!LD1; // toggle LD1
+  while (!USER) {
+    LD1=0; // toggle LD1
   }
 }
